844a52721b7f7790057c2d17a3299808fe5b2eaa
Luis Henrique Silva Santos <luissantosus2@gmail.com>
2026-04-24 11:07:03 -0300

    Update Biblioteca.java

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,27 +1,41 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Biblioteca {
-    private ArrayList<MaterialBiblioteca>  materiais;
+    private ArrayList<MaterialBiblioteca> materiais;
 
-    //Iniciando a lista de array por meio de um metoto
     Biblioteca() {
         materiais = new ArrayList<>();
     }
-    
-    public void adicionarMaterial(MaterialBiblioteca material){
+
+    public void adicionarMaterial(MaterialBiblioteca material) {
         materiais.add(material);
     }
 
     public void listaMateriais() {
-        for(MaterialBiblioteca m : materiais){
+        for (MaterialBiblioteca m : materiais) {
             m.exibirDetalhes();
         }
     }
-    buscarPorTitulo(String titulo) {
-        for(MaterialBiblioteca m : materiais){
-            if(material.getTitulo())
+
+    public MaterialBiblioteca buscarPorTitulo(String titulo) {
+        for (MaterialBiblioteca m : materiais) {
+            if (m.getTitulo().equalsIgnoreCase(titulo)) {
+                return m;
+
+            }
         }
+        System.out.println("Material não encontrado");
+        return null;
     }
 
+    public void emprestarMaterial(MaterialBiblioteca material) {
+
+        if (material != null) {
+            material.emprestar();
+            materiais.remove(material);
+
+        } else {
+            System.out.printf("Material não encontrado");
+        }
+    }
 }

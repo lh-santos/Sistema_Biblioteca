280f545dc15616b8c9e7ada3c32d45b2be825079
Luis Henrique Silva Santos <luissantosus2@gmail.com>
2026-04-24 11:07:32 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,31 +3,97 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         int op;
+        boolean continuar = true;
+        Scanner leitor = new Scanner(System.in);
 
         Biblioteca b1 = new Biblioteca();
 
-        Scanner leitor = new Scanner(System.in);  
-        System.out.println("===== MENU BIBLIOTECA =====");
-        System.out.println("1 - Adicionar Livro");
-        System.out.println("2 - Adicionar Revista");
-        System.out.println("3 - Adicionar Jornal");
-        System.out.println("4 - Listar Materiais");
-        System.out.println("5 - Buscar por Título");
-        System.out.println("6 - Emprestar Material");
-        System.out.println("7 - Sair");
-        op = leitor.nextInt();
+        while(continuar) {
+
+            System.out.printf("===== MENU BIBLIOTECA =====");
+            System.out.printf("1 - Adicionar Livro");
+            System.out.printf("2 - Adicionar Revista");
+            System.out.printf("3 - Adicionar Jornal");
+            System.out.printf("4 - Listar Materiais");
+            System.out.printf("5 - Buscar por Título");
+            System.out.printf("6 - Emprestar Material");
+            System.out.printf("7 - Sair");
+            op = leitor.nextInt();
+            leitor.nextLine();
 
 
-        while(true) {
-            switch (op) {
-                case 1:
-                    leitor.adicionarMaterial(material);
-                    break;
-            
-                case 2:
 
-                    break;
-                    
+                switch (op) {
+                    case 1:
+                        System.out.printf("Titulo: ");
+                        String tituloLivro = leitor.nextLine();
+
+                        System.out.printf("Autor: ");
+                        String autorLivro = leitor.nextLine();
+
+                        System.out.printf("Ano: ");
+                        int anoLivro = leitor.nextInt();
+
+                        System.out.printf("Numero de paginas: ");
+                        int pagLivro = leitor.nextInt();
+
+                        b1.adicionarMaterial(tituloLivro, anoLivro, autorLivro, pagLivro);
+                        break;
+
+                    case 2:
+                        System.out.printf("Titulo: ");
+                        String tituloRevista = leitor.nextLine();
+
+                        System.out.printf("Autor: ");
+                        String autorRevista = leitor.nextLine();
+
+                        System.out.printf("Ano: ");
+                        int anoRevista = leitor.nextInt();
+
+                        System.out.printf("Numero de edição: ");
+                        int edRevista = leitor.nextInt();
+
+                        b1.adicionarMaterial(tituloRevista, anoRevista, autorRevista, edRevista);
+                        break;
+
+
+                    case 3:
+                        System.out.printf("Titulo: ");
+                        String tituloJornal = leitor.nextLine();
+
+                        System.out.printf("Autor: ");
+                        String autorJornal = leitor.nextLine();
+
+                        System.out.printf("Ano: ");
+                        int anoJornal = leitor.nextInt();
+
+                        System.out.printf("Numero de paginas: ");
+                        int dataEdicaoJornal = leitor.nextInt();
+
+                        b1.adicionarMaterial(tituloJornal, anoJornal, autorJornal, dataEdicaoJornal);
+                        break;
+
+                    case 4:
+                        b1.listaMateriais();
+
+                        break;
+
+                    case 5:
+                        System.out.printf("Informe o nome do material: ");
+                        b1.buscarPorTitulo();
+                        break;
+
+                    case 6:
+
+                        break;
+
+                    case 7:
+                        System.out.printf("Encerrando...");
+
+                        continuar = false;
+
+                        break;
+
             }
         }
     }
